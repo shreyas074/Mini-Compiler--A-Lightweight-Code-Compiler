f594ae4f45386d745bb0f6f8a1b3e4e546336e62
shreyas074 <149076510+shreyas074@users.noreply.github.com>
2025-05-20 19:59:24 +0000

    Add files via upload

---
+++ Semantic Analysis/test.c
@@ -0,0 +1,44 @@
+
+
+int fun(int a, int b, int c)
+{
+  return 2 ;
+}
+
+int main()
+{
+
+  // int arr[-1];
+  // int arr1[0];
+  // int arr2[-1];
+  // break;
+  // arr[0] = 4;
+  // arr[-1] = 10;
+  // arr[11] = 2;
+
+  int x = 1;
+
+  int y;
+  int z;
+
+  if(x<0)
+  {
+    int y;
+
+    int z=1;
+
+    {
+      int w =1;
+    }
+    //w=3;
+  }
+  char c='a';
+  for(x=2;x<y;x++)
+  {
+    break;
+  }
+  return 3;
+
+  // fun();
+
+}
