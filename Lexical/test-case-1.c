a2f7250720d72d62537e1956660c390c65f09189
himanshu0614 <0614himanshusingh@gmail.com>
2025-05-21 21:53:21 +0530

    Add files via upload
    added the lexical analysis phase

---
+++ Lexical/test-case-1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+    // Single line comment
+    
+    /* Multi-line comment
+       Like this */
+    
+    /* here */ int a; /* "int a" should be untouched */
+    
+	// This nested comment // This comment should be removed should be removed
+
+
+	return 0;
+}
